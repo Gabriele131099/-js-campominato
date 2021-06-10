db590d7cf0328f745fa7d64ab2b5ab20e931bac0
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-10 20:33:09 +0200

    struttura

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ var arrayUtente = []
 var nCasuale = 16;
 var x=100
 var score = 0;
+document.getElementById("arrayNum").innerHTML = arrayUtente + " </br> " + arrayNum + " </br> " + "score: " + score
 
 for (var i = 0; i < nCasuale; i++) {
     arrayNum.push(Math.floor(Math.random()*nCasuale)) 
 }
-while (x>0) {
-    var utenteNumero = prompt("inserisci un numero")
-    if (arrayNum.includes(utenteNumero)) {
-        alert("perso" + sconto) 
-        break;  
-    } else if (arrayUtente.includes(utenteNumero)) {
-        alert("il numero è gia presente")
+function gioca() {
+
+    while (x>0) {
+        var utenteNumero = prompt("inserisci un numero")
+        if (arrayNum.includes(utenteNumero)) {
+            alert("perso" + sconto) 
+            break;  
+        } else if (arrayUtente.includes(utenteNumero)) {
+            alert("il numero è gia presente")
+            
+        } else {
+            arrayUtente.push(utenteNumero)
+            score += 15
+            x-=16
+        }
         
-    } else {
-        arrayUtente.push(utenteNumero)
-        score += 15
-        x-=16
     }
+    alert("Il tuo punteggio è " + score)
+    document.getElementById("arrayNum").innerHTML = arrayUtente + " </br> " + arrayNum + " </br> " + "score: " + score
     
 }
-alert("Il tuo punteggio è " + score)
-document.getElementById("arrayNum").innerHTML = arrayUtente + " </br> " + arrayNum + " </br> " + "score: " + score
