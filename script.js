e8cfb68a628ac76dfeb4fb45321d187c9d02c8ff
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-10 19:38:41 +0200

    struttura

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ var arrayNum = []
 var arrayUtente = []
 var nCasuale = 16;
 for (var i = 0; i < 16; i++) {
-    arrayNum.push(Math.floor(Math.random()*nCasuale))
-    
+    arrayNum.push(Math.floor(Math.random()*nCasuale)) 
 }
 var x=100
 var score = 0;
 while (x>0) {
     var utenteNumero = prompt("inserisci un numero")
     if ( arrayUtente.includes(utenteNumero)) {
-        alert("Hai perso , ri prova sarai più sfortunato ")
+        alert("Il numero è già presente") 
+    } if (arrayNum.includes(utenteNumero)) {
+        alert("hai perso")
+        break;
     } else {
         arrayUtente.push(utenteNumero)
         score += 15
-
     }
     x-=16
 }
-
 alert("Il tuo punteggio è " + score)
