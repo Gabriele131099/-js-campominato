7a360555265b79163441e2f9cca31d95a2c41e03
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-10 19:04:09 +0200

    struttura

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ for (var i = 0; i < 16; i++) {
     
 }
 var x=100
+var score = 0;
 while (x>0) {
     var utenteNumero = prompt("inserisci un numero")
     if ( arrayUtente.includes(utenteNumero)) {
         alert("Hai perso , ri prova sarai più sfortunato ")
     } else {
         arrayUtente.push(utenteNumero)
+        score += 15
 
     }
     x-=16
 }
+
+alert("Il tuo punteggio è " + score)
