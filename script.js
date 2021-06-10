20f89d3a198e1bc7f726d0c3b54112e481c808c7
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-10 19:59:02 +0200

    struttura

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ var x=100
 var score = 0;
 while (x>0) {
     var utenteNumero = prompt("inserisci un numero")
-    if ( arrayUtente.includes(utenteNumero)) {
-        alert("Il numero è già presente") 
-    } if (arrayNum.includes(utenteNumero)) {
-        alert("hai perso")
-        break;
+    if (arrayNum.includes(utenteNumero)) {
+        alert("perso" + sconto) 
+        break;  
+    } else if (arrayUtente.includes(utenteNumero)) {
+        alert("il numero è gia presente")
+        
     } else {
         arrayUtente.push(utenteNumero)
         score += 15
+        x-=16
     }
-    x-=16
+    
 }
 alert("Il tuo punteggio è " + score)
