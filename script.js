25ee9810c6553f906b8dfc1a8212750f602823be
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-11 12:30:10 +0200

    struttura

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var arrayUser = [];
 
 //creo array numeri casuali
 
-//Funzione che dato in ingresso un array e un elemento, verifica se l'elelmento è presente all'interno dell'array
-function isInArray(array, element) {
+//Funzione, verifica se l'elelmento è presente all'interno dell'array
+function checkArray(array, element) {
   for ( var x = 0 ; x < array.length ; x++){
     if(element === array[x]){
       return true;
@@ -15,13 +15,13 @@ function isInArray(array, element) {
 }
 while(arrayNum.length<16){
     var numRandom = (Math.floor(Math.random()*100)+1);
-    if(!(isInArray(arrayNum, numRandom))){
+    if(!(checkArray(arrayNum, numRandom))){
         arrayNum.push(numRandom);
     }
 }
 
-//Funzione che dato un messaggio in ingresso, restituisce il numero inserito dall'utente se e solo se è un numero, altrimenti ripete la richiesta
-function insertNumber(message){
+//Funzione,accetta il numero inserito dall'utente se e solo se è un numero, altrimenti ripete la richiesta
+function nUtente(message){
     var num = parseInt(prompt(message));
     while(isNaN(num)){
         var num = parseInt(prompt("Attenzione il valore deve essere numerico"));
@@ -30,14 +30,14 @@ function insertNumber(message){
 }
 // array user , richiesta numeri utenti 
 while(arrayUser.length<84){
-    var insertNum = insertNumber("Inserisci il numero");
-    if(!(isInArray(arrayNum, insertNum))){
-        if(!(isInArray(arrayUser, insertNum))){
-            arrayUser.push(insertNum);
+    var nUtente = nUtente("Inserisci il numero");
+    if(!(checkArray(arrayNum, nUtente))){
+        if(!(checkArray(arrayUser, nUtente))){
+            arrayUser.push(nUtente);
 
         }else{
             alert("il numero gia è stato inserito")
-            insertNum = insertNumber("Inserisci il numero");
+            nUtente = nUtente("Inserisci il numero");
         }
     }else{
         break
