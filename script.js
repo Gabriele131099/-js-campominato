d9c4018c6665a5d8bfd3e68639a3ab28652957ce
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-11 12:59:28 +0200

    struttura

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var arrayNum = [];
 var arrayUser = [];
 
 
+
 //Funzione, verifica se l'elelmento è presente all'interno dell'array
 function isInArray(array, element) {
     for ( var x = 0 ; x < array.length ; x++){
@@ -19,7 +20,7 @@ while(arrayNum.length<16){
         arrayNum.push(numRandom);
     }
 }
-
+arrayNum.sort()
 //Funzione,accetta il numero inserito dall'utente se e solo se è un numero, altrimenti ripete la richiesta
 function nUtente(message){
     var num = parseInt(prompt(message));
@@ -28,25 +29,32 @@ function nUtente(message){
     }
     return num;
 }
+
 // array user , richiesta numeri utenti 
 var score = 0
-while(arrayUser.length<84){
-    var numbUser = nUtente("Inserisci il numero");
-    if(!(isInArray(arrayNum, numbUser))){
-        if(!(isInArray(arrayUser, numbUser))){
-            arrayUser.push(numbUser);
-            score += 15
+function gioca() {
+
+    while(arrayUser.length<84){
+        var numbUser = nUtente("Inserisci il numero");
+        if(!(isInArray(arrayNum, numbUser))){
+            if(!(isInArray(arrayUser, numbUser))){
+                arrayUser.push(numbUser);
+                score += 15
+
+            }else{
+                alert("il numero gia è stato inserito")
+                numbUser = nUtente("Inserisci il numero");
+            }
         }else{
-            alert("il numero gia è stato inserito")
-            numbUser = nUtente("Inserisci il numero");
+            alert("hai persoooooo")
+            break
         }
-    }else{
-        break
     }
+    
+    print.innerHTML +=" "+  + score  + "</br> I tuoi numeri sono: " + arrayUser + "</br> Le mine hanno i seguenti numeri: " + arrayNum;
+    
 }
 
-
-print.innerText += score;
 
 
 
